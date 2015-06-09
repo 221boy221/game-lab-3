af95800567a3c46fddb70b350db11e2cabdc6a8a
BeauArthurs <BeauArthurs@gmail.com>
2015-06-09 17:06:29 +0200

    Beau_005

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -18,15 +18,34 @@ public class Player : MonoBehaviour {
     public AnimatorStateInfo currentBaseState;
     private Rigidbody2D body;
     private NetworkView _networkView;
-    void FixedUpdate() {
-        if (haveControl) {
+    private bool inRange;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        speed = 5;
+        health = 100;
+        attacked = false;
+        midAttack = Animator.StringToHash("AttackMid");
+        lowAttack = Animator.StringToHash("AttackLow");
+        lowBlock = Animator.StringToHash("BlockMid");
+        midBlock = Animator.StringToHash("BlockLow");
+        body = GetComponent<Rigidbody2D>();
+        _networkView = GetComponent<NetworkView>();
+
+    }
+    void FixedUpdate() 
+    {
+        if (haveControl) 
+        {
             horiz = Input.GetAxis("Horizontal");
             Vector2 newVelocity = (transform.right * horiz * moveSpeed);
-            Vector2 myVelocity = GetComponent<Rigidbody2D>().velocity;
+            Vector2 myVelocity = body.velocity;
             myVelocity.x = newVelocity.x;
 
-            if (myVelocity != GetComponent<Rigidbody2D>().velocity) {
-                if (Network.isServer) {
+            if (myVelocity != body.velocity)
+            {
+                if (Network.isServer) 
+                {
                     movePlayer(myVelocity);
                 } else {
                     GetComponent<NetworkView>().RPC("movePlayer", RPCMode.Server, (Vector3)myVelocity);
@@ -34,10 +53,77 @@ public class Player : MonoBehaviour {
             }
         }
     }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            anim.SetBool("BlockMid", true);
+        }
+        else if (Input.GetKeyDown(KeyCode.Q))
+        {
+            anim.SetBool("BlockLow", true);
+        }
+        if (Input.GetKeyUp(KeyCode.Q))
+        {
+            anim.SetBool("BlockLow", false);
+        }
+        if (Input.GetKeyUp(KeyCode.E))
+        {
+            anim.SetBool("BlockMid", false);
+        }
+        if (Input.GetMouseButtonDown(0))
+        {
+            anim.SetTrigger("AttackLow");
+            attacked = true;
+            if (attacked == false && enemy != null)
+            {
+                enemy.damage(10, 0);
+                Debug.Log(enemy.health);
+            }
+            attacked = true;
+        }
+        else if (Input.GetMouseButtonDown(1))
+        {
+            anim.SetTrigger("AttackMid");
+            Debug.Log(attacked + ":" + enemy);
+            if (attacked == false && enemy != null)
+            {
+                enemy.damage(10, 1);
+                Debug.Log(enemy.health);
+            }
+            attacked = true;
+        }
+        currentBaseState = anim.GetCurrentAnimatorStateInfo(0);
 
+
+        if (currentBaseState.fullPathHash != lowAttack && currentBaseState.fullPathHash != midAttack)
+        {
+            attacked = false;
+        }
+    }
+    public void damage(int amount, int midLow)
+    {
+        if (midLow == 0 && currentBaseState.fullPathHash != lowBlock)
+        {
+            health -= amount;
+        }
+        else if (midLow == 1 && currentBaseState.fullPathHash != midBlock)
+        {
+            health -= amount;
+        }
+        Debug.Log(health);
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        enemy = other.GetComponent<Player>();
+    }
+    void OnTriggerExit2D()
+    {
+        enemy = null;
+    }
     [RPC]
     void movePlayer(Vector3 playerVelocity) {
-        GetComponent<Rigidbody2D>().velocity = playerVelocity;
+        body.velocity = playerVelocity;
         GetComponent<NetworkView>().RPC("updatePlayer", RPCMode.OthersBuffered, transform.position);
     }
 
